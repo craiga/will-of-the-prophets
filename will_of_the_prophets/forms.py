b9229d2c0161812b587e34712e1b428ba30001a8
Craig Anderson <craiga@craiga.id.au>
2018-06-28 17:45:49 +0100

    Add datetime picker

--- will_of_the_prophets/forms.py
+++ will_of_the_prophets/forms.py
@@ -8,8 +8,6 @@ from will_of_the_prophets.models import Roll
 class RollForm(forms.ModelForm):
     """Roll form."""
 
-    embargo = forms.SplitDateTimeField()
-
     class Meta:
         model = Roll
         fields = ['embargo']
