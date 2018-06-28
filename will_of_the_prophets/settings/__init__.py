82907fb8e516a37e0cd04bb7f4910d3fcbe21bce
Craig Anderson <craiga@craiga.id.au>
2018-06-28 09:17:08 +0100

    Adding Sentry

--- will_of_the_prophets/settings/__init__.py
+++ will_of_the_prophets/settings/__init__.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'raven.contrib.django.raven_compat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
