0c7dc9987eedb54fb654b4a57416a44ab56d331c
Craig Anderson <craiga@craiga.id.au>
2018-07-01 10:33:38 +0100

    Add debug toolbar and don't make thousands of queries.

--- will_of_the_prophets/tests/test_calculate_position.py
+++ will_of_the_prophets/tests/test_calculate_position.py
@@ -2,26 +2,31 @@
 
 import pytest
 
-from will_of_the_prophets.board import calculate_position
+from will_of_the_prophets import board
 from factories import ButtholeFactory
+
+
+@pytest.fixture(autouse=True)
+def clear_caches():
+    board.clear_caches()
 
 
 @pytest.mark.django_db
 def test_zero_rolls():
-    assert calculate_position() == 1
+    assert board.calculate_position() == 1
 
 
 @pytest.mark.django_db
 def test_one_roll():
-    assert calculate_position(22) == 23
+    assert board.calculate_position(22) == 23
 
 
 @pytest.mark.django_db
 def test_many_rolls():
-    assert calculate_position(3, 20, 2, 40, 17, 5) == 88
+    assert board.calculate_position(3, 20, 2, 40, 17, 5) == 88
 
 
 @pytest.mark.django_db
 def test_butthole():
     ButtholeFactory(start_square=88, end_square=5)
-    assert calculate_position(3, 20, 2, 40, 17, 5, 2) == 7
+    assert board.calculate_position(3, 20, 2, 40, 17, 5, 2) == 7
