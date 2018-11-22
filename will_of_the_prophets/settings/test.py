d9dc85c0f6979ed380d9afec4fee4ec6bb037d32
Craig Anderson <craiga@craiga.id.au>
2018-11-22 15:12:09 +0000

    Move comment to keep code climate happy.

--- will_of_the_prophets/settings/test.py
+++ will_of_the_prophets/settings/test.py
@@ -2,7 +2,8 @@
 Settings for tests.
 """
 
+# pylint: disable=wildcard-import, unused-wildcard-import
 
-from will_of_the_prophets.settings import *  # pylint: disable=wildcard-import, unused-wildcard-import
+from will_of_the_prophets.settings import *
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
