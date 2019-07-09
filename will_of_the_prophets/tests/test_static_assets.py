2a64f2ddd5ba67f09bd4bc1d71384b15443fdf1a
Craig Anderson <craiga@craiga.id.au>
2019-07-09 17:04:03 +0100

    Fix tests for when debug is disabled

--- will_of_the_prophets/tests/test_static_assets.py
+++ will_of_the_prophets/tests/test_static_assets.py
@@ -6,5 +6,5 @@ import pytest
 @pytest.mark.parametrize("url", ["/favicon.ico", "/robots.txt", "/humans.txt"])
 def test_static_asset(client, url):
     """Test that static assets are present."""
-    response = client.get(url, follow_redirects=True)
+    response = client.get(url, secure=True)
     assert response.status_code == 200
