0f20a66c6acb9ece5fd543034eee5a06d0f17e3a
Craig Anderson <craiga@craiga.id.au>
2021-01-26 13:56:34 +0000

    Add different die sizes to roll form

--- will_of_the_prophets/forms.py
+++ will_of_the_prophets/forms.py
@@ -1,5 +1,7 @@
 """Will of the Prophets forms."""
 
+import random
+
 from django import forms
 
 from will_of_the_prophets.models import Roll
@@ -8,7 +10,21 @@ from will_of_the_prophets.models import Roll
 class RollForm(forms.ModelForm):
     """Roll form."""
 
+    die_size = forms.ChoiceField(choices=[(6, "Six-sided die"), (100, "100-sided die")])
+
+    def save(self, commit=True):
+        """
+        Save the roll with a random number between 1 and the selected die size.
+        """
+        obj = super().save(commit=False)
+        obj.number = random.randint(1, int(self.cleaned_data["die_size"]))
+
+        if commit:
+            obj.save()
+
+        return obj
+
     class Meta:
         model = Roll
-        fields = ["embargo"]
-        labels = {"embargo": "Date and time the next move will be public"}
+        fields = ["embargo", "die_size"]
+        labels = {"embargo": "Date and time move will be public"}
