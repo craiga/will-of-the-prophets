eaa2237e829955c749e7cb01ffb08d13555bca49
Craig Anderson <craiga@craiga.id.au>
2024-05-06 11:52:04 +0100

    Roll timestamp has time.

--- cypress/e2e/roll.cy.js
+++ cypress/e2e/roll.cy.js
@@ -4,7 +4,7 @@ describe("Rolling", () => {
     cy.contains("Username").click().type("rollinguser");
     cy.contains("Password").click().type("rolling user password");
     cy.contains("Log In").click();
-    cy.get("input[name='embargo']").type("2050-12-31");
+    cy.get("input[name='embargo']").type("2050-12-31T00:00");
     cy.get("button[type='submit']").click();
     cy.contains("Board as of Dec. 31, 2050");
   });
